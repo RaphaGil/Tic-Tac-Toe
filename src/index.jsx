37c958743fdd568073eab78f226c5a5deacfaa21
raphaelagil <raphaela.gil@yahoo.com>
2024-05-19 15:24:03 +0100

    changed the index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,13 @@
-import ReactDOM from 'react-dom/client.js';
+// import ReactDOM from 'react-dom/client.js';
 
-import App from './App.jsx';
+// import App from './App.jsx';
+// import './index.css';
+
+// ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+import React from 'react';
+import ReactDOM from 'react-dom/client'; // Note the '/client' part
+import App from './App';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
